c3117322c411798e1e9414f8effff92e5317dd5e
Kazui Fukushima <fronta.kazui.1429@gmail.com>
2023-06-23 18:45:22 +0000

    [インデント]

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,9 +1,9 @@
 class Comment < ApplicationRecord
-  
+
   belongs_to :user
   belongs_to :post
-  
-  validates :comment_introduction, presence: true
+
+  validates :comment_introduction, presence: true, length: {maximum: 120}
   validates :total_score, presence: true
   validates :read_score, presence: true
   validates :price_score, presence: true
