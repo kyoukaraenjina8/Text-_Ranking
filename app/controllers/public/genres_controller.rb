06656fdf7f612cabc3a33462d747c6a8f3519716
Kazui Fukushima <fronta.kazui.1429@gmail.com>
2023-06-28 17:00:50 +0000

    [コントローラ修正]

--- app/controllers/public/genres_controller.rb
+++ app/controllers/public/genres_controller.rb
@@ -1,13 +1,13 @@
 class Public::GenresController < ApplicationController
   
   def show
-    @genres = Genre.all.order(created_at: :desc)
+    @genres = Genre.page(params[:page]).order(created_at: :desc)
     @genre = Genre.find(params[:id])
     #@post = @genre.posts.page(params[:page])
+
   end
   
   def genre_rank
-    #@posts = @genre.posts.order(created_at: :desc)
     @genres = Genre.all
     @genre = Genre.find(params[:id])
     @genre_rankings = @genre.rank_genre
