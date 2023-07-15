5804a56c3cb9584584cb05c569c1f087aeaee05c
Kazui Fukushima <fronta.kazui.1429@gmail.com>
2023-07-15 07:04:51 +0000

    [favorites]

--- app/controllers/public/users_controller.rb
+++ app/controllers/public/users_controller.rb
@@ -7,8 +7,17 @@ class Public::UsersController < ApplicationController
   def show
     @user =User.find(params[:id])
     @user_post = @user.posts
+    favorites= Favorite.where(user_id: @user.id).pluck(:post_id)
+    @favorite_posts = Post.find(favorites)
   end
 
+  def index
+    @user =User.find(params[:id])
+    @user_post = @user.posts
+    favorites= Favorite.where(user_id: @user.id).pluck(:post_id)
+    @favorite_posts = Post.find(favorites)
+  end
+  
   def edit
     @user_edit = current_user
   end
@@ -22,7 +31,7 @@ class Public::UsersController < ApplicationController
       render 'edit'
     end
   end
-
+  
   def confirm_withdraw
   end
 
