ce5bd773b25d4a6a256f7aca9cee1df7e9266015
Kazui Fukushima <fronta.kazui.1429@gmail.com>
2023-07-20 11:48:17 +0000

    [favorites完了]

--- config/routes.rb
+++ config/routes.rb
@@ -16,22 +16,40 @@ devise_scope :user do
     post 'public/guest_sign_in', to: 'public/sessions#guest_sign_in'
   end
 
-  namespace :public do
-    get 'users/confirm_withdraw'
-  end
+# scope module: :public do
+#   get '/searches/search' => 'searches#search'
+#   get '/posts/rank' => 'posts#rank'
+#   get 'users/confirm_withdraw' => 'users#confirm_withdraw', as: 'confirm_withdraw'
+#   patch '/users/withdraw' => 'users#withdraw'
+#   get 'genres/:id/genre_rank' => 'genres#genre_rank', as: 'genre_rank'
+#   resources :genres, only: [:show]
+#   resources :users, only: [:show, :edit,:update]
+#   resource :posts, only: [:new,:create]
+#   resources :posts, only: [:show,:index,:destroy,:edit,:update] do
+#     resource :favorites, only: [:create, :destroy]
+#     resources :comments, only: [:create,:destroy,:new]
+#   end
+# end
 
 scope module: :public do
   get '/searches/search' => 'searches#search'
-  get '/posts/rank' => 'posts#rank'
-  get 'users/confirm_withdraw' => 'users#confirm_withdraw', as: 'confirm_withdraw'
-  patch '/users/withdraw' => 'users#withdraw'
-  get 'genres/:id/genre_rank' => 'genres#genre_rank', as: 'genre_rank'
-  resources :genres, only: [:show]
-  resources :users, only: [:show, :edit,:update]
-  resource :posts, only: [:new,:create]
-  resources :posts, only: [:show,:index,:destroy,:edit,:update] do
-    resource :favorites, only: [:create, :destroy]
+  resources :genres, only: [:show] do
+    member do 
+      get 'genre_rank'
+    end
+  end
+  resources :users, only: [:show,:edit,:update] do
+    collection do 
+      get 'confirm_withdraw'
+      patch 'withdraw'
+    end
+  end
+  resources :posts do
+    collection do
+      get 'rank'
+    end
     resources :comments, only: [:create,:destroy,:new]
+    resource :favorites, only: [:create, :destroy]
   end
 end
 
@@ -42,7 +60,6 @@ namespace :admin do
   patch '/users/withdraw' => 'users#withdraw'
   get 'users/:id/comment_index' => 'users#comment_index'
   resources :genres, only: [:index, :show, :edit, :create, :update]
-
 
 end
 
